2bc890c0121a6bd76d01366093527f7537888a94
vovapankiw <vovapankiw@gmail.com>
2020-10-03 09:47:56 +0300

    added Http class

---
+++ models/Http.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class HTTP {
+  constructor(url) {
+    const baseUrl = url || process.env.BASE_URL || 'http://localhost:3000';
+    console.log('baseUrl', baseUrl);
+    this.instance = axios.create({
+      url: baseUrl,
+    });
+  }
+
+  fetch(url, options) {
+    return this.instance.get(url, options);
+  }
+
+  create(url, data, options) {
+    return this.instance.post(url, data, options);
+  }
+
+  update(url, data, options) {
+    return this.instance.put(url, data, options);
+  }
+
+  remove(url, options) {
+    return this.instance.delete(url, options);
+  }
+}
+
+export default HTTP;
